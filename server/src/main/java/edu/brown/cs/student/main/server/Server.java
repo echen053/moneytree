b3909be538c9e286002b82db0f7f857ab30308cb
Emma Chen <echen053@gmail.comgit config --global user.name emma chen>
2025-01-19 00:20:03 -0500

    changed cors

--- server/src/main/java/edu/brown/cs/student/main/server/Server.java
+++ server/src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -1,7 +1,6 @@
 package edu.brown.cs.student.main.server;
 
 import static spark.Spark.after;
-import static spark.Spark.before;
 
 import edu.brown.cs.student.main.server.handlers.AddHandler;
 import edu.brown.cs.student.main.server.handlers.DeleteHandler;
@@ -25,14 +24,14 @@ public class Server {
     Spark.port(port);
 
     // Add a CORS filter
-    before(
-        (request, response) -> {
-          response.header("Access-Control-Allow-Origin", "*");
-          response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-          response.header(
-              "Access-Control-Allow-Headers",
-              "Content-Type, Authorization, Content-Length, X-Requested-With");
-        });
+    //    before(
+    //        (request, response) -> {
+    //          response.header("Access-Control-Allow-Origin", "*");
+    //          response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    //          response.header(
+    //              "Access-Control-Allow-Headers",
+    //              "Content-Type, Authorization, Content-Length, X-Requested-With");
+    //        });
 
     after(
         (Filter)
